7edb144219d4c7b26513bf80fa94303943635ceb
Alisson Lima <alissonpcl@outlook.com>
2020-01-03 00:43:11 -0300

    Commit inicial

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alissonlima/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alissonlima/Documents/Projects/IntelliJ/Flutter/Curso_Udemy/Projetos/loja_virtual"
+export "FLUTTER_TARGET=/Users/alissonlima/Documents/Projects/IntelliJ/Flutter/Curso_Udemy/Projetos/loja_virtual/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/alissonlima/Library/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
